3be149e7fb45a55dbceb64d2f6c723f76c94663b
Shahinur Islam <shahinur.islam@ideascale.com>
2025-08-31 12:22:53 +0600

    remove Header component from App and simplify structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import {Header} from './components'
 import Icons from "./components/icons/Icons.tsx";
 
 function App() {
 
   return (
     <div className="app">
-      <Header 
-        title="Vite + React + Bootstrap" 
-        subtitle="Built with TypeScript, Reactstrap & SCSS"
-      />
-
       <main className="main-content">
         <Icons/>
       </main>
